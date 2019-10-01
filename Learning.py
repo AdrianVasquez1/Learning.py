fa030aebc82cd74ec77796a2f7427ba0e2f366c3
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-01 08:23:40 -0500

    Playing with the Maths

--- Learning.py
+++ Learning.py
@@ -5,9 +5,15 @@ print("I like typing this")
 print("this is fun")
 
 # math skills demo
+# prints the following sentence
 print("I will not count my chickens",  2)
+# calculates the  equation giving the number of hens
 print("Hens: ", 25+30/6)
+# calculates the number of roosters in the equation
 print("Roosters: ", 100 - 25 * 3 % 4)
+# prints the phrase "I will count the eggs
 print("Now I will count the eggs", 7)
+# solves the equation for the number of eggs
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# Solves a greater than less than equation
 print(3 + 2 < 5 - 7)
