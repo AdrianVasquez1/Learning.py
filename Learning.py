5a3f33c98ac3bf1e3c3981d7459cee4487e16450
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-01 08:34:35 -0500

    Using code to solve math and type responses

--- Learning.py
+++ Learning.py
@@ -8,9 +8,9 @@ print("this is fun")
 # prints the following sentence
 print("I will not count my chickens", 2)
 # calculates the  equation giving the number of hens
-print("Hens: ", 25 + 30 / 6)
+print("Hens: ", (.25 * 10) + 30 / 6)
 # calculates the number of roosters in the equation
-print("Roosters: ", 100 - 25 * 3 % 4)
+print("Roosters: ", (10 * 10) - 25.0 * 3 % 4)
 # prints the phrase "I will count the eggs
 print("Now I will count the eggs", 7)
 # solves the equation for the number of eggs
@@ -18,4 +18,4 @@ print(+ 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # Solves a greater than less than equation
 print(3 + 2 < 5 - 7)
 print("I will now count my chicks and geese")
-print(50 - 25 + 5 - 10)
+print(50.0 - 25.0 + 5.0 - 10.0)
