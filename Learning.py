eae2b15702332a9bc864309d845b75e2e151444d
AdrianVasquez1 <100114395@lubbockisd.net>
2019-10-03 08:37:39 -0500

    Added some practice with variables

--- Learning.py
+++ Learning.py
@@ -19,3 +19,20 @@ print(+ 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print(3 + 2 < 5 - 7)
 print("I will now count my chicks and geese")
 print(50.0 - 25.0 + 5.0 - 10.0)
+
+# Variables and some of their powers
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = space_in_a_car * cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("there are", drivers, "drivers available today.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put approximately", average_passengers_per_car, "in each car.")
